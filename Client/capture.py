0f86fb3ed6bb7d7f12ab77e8da8fde434bd720bd
N4thancy <93983753+N4thancy@users.noreply.github.com>
2022-12-14 18:25:25 +0100

    Update capture.py

--- Client/capture.py
+++ Client/capture.py
@@ -3,21 +3,20 @@ from time import sleep
 
 camera = PiCamera()
 
-class capture_info:
+class Capture_info:
     fol = "/home/pi/Desktop/Code/pic/"
-#camera.start_preview()
     ext = "0001"
     ftype = ".jpg"
 
 def capture():
-    path = capture_info.fol + capture_info.ext + capture_info.ftype
+    path = Capture_info.fol + Capture_info.ext + Capture_info.ftype
     print(path)
-    print(capture_info.ext)
+    print(Capture_info.ext)
     camera.capture(path)
-    ext = int(capture_info.ext) +1
+    ext = int(Capture_info.ext) +1
     print(len(str(ext)))
-    capture_info.ext = "0" * (4 - len(str(ext)))+ str(ext)
-    sleep(0.5)
+    Capture_info.ext = "0" * (4 - len(str(ext)))+ str(ext)
+    sleep(0.2)
     
 
 #camera.stop_preview()
